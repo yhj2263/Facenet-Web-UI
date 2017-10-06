d1ef1164f71e430d2a82224c41daf9557fb7abe9
YHJ <yhj2263>
2017-10-06 10:53:49 -0700

    reading port from a global variable

--- src/services/Api.js
+++ src/services/Api.js
@@ -4,7 +4,8 @@ export default function () {
   const port = process.env.ServerPort
   console.log(port)
   return axios.create({
-    baseURL: 'http://54.237.168.141:8081'
+    // baseURL: 'http://54.237.168.141:8081'
     // baseURL: 'http://localhost:8081'
+    baseURL: port
   })
 }
