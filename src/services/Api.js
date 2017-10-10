4c4e665ffed48c951cb0d01d1edb6c9939bbc9fa
YHJ <yhj2263>
2017-10-09 18:17:54 -0700

    changed the ip address to server ip

--- src/services/Api.js
+++ src/services/Api.js
@@ -4,7 +4,7 @@ export default function () {
   const port = process.env.ServerPort
   console.log(port)
   return axios.create({
-    // baseURL: 'http://54.237.168.141:8081'
-    baseURL: 'http://localhost:8081'
+    baseURL: 'http://54.237.168.141:8081'
+    // baseURL: 'http://localhost:8081'
   })
 }
